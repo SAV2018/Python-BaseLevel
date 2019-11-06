0b1db19ef9541ae96f4673be6f2abf47aa2ccae2
Sav <arconar@mail.ru>
2019-11-07 01:10:31 +0300

    Lesson 1-4

--- BL-L4/task-4-1.py
+++ BL-L4/task-4-1.py
@@ -4,3 +4,12 @@
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 '''
 
+from sys import argv
+
+scriptname, name, worktime, rate_per_hour, bonus = argv
+print(f'Payroll for {name}: {worktime} hours worked, rate ${rate_per_hour} per hour, bonus ${bonus}')
+
+try:
+    print(f'\nSalary of {name}: ${round(float(worktime) * float(rate_per_hour) + float(bonus), 2):.2f}')
+except ValueError:
+    print('Error: Incorrect value of parameter')
