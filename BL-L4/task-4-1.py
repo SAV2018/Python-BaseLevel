e8c686e7ae8d9f27c406714860c355b19e4ace6e
Sav <arconar@mail.ru>
2019-11-07 23:54:52 +0300

    Lesson 1-4

--- BL-L4/task-4-1.py
+++ BL-L4/task-4-1.py
@@ -6,10 +6,10 @@
 
 from sys import argv
 
-scriptname, name, worktime, rate_per_hour, bonus = argv
-print(f'Payroll for {name}: {worktime} hours worked, rate ${rate_per_hour} per hour, bonus ${bonus}')
-
 try:
+    scriptname, name, worktime, rate_per_hour, bonus = argv
+    print(f'Payroll for {name}: {worktime} hours worked, rate ${rate_per_hour} per hour, bonus ${bonus}')
+
     print(f'\nSalary of {name}: ${round(float(worktime) * float(rate_per_hour) + float(bonus), 2):.2f}')
 except ValueError:
     print('Error: Incorrect value of parameter')
