c36c0cba0c54d38bf0d950ba25ccaf15f0cdcbb6
Sav <arconar@mail.ru>
2019-11-09 03:23:40 +0300

    Lesson 1-5

---
+++ BL-L5/task-5-5.py
@@ -0,0 +1,17 @@
+'''
+Создать (программно) текстовый файл, записать в него программно набор чисел, разделенных пробелами.
+Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+'''
+from random import randint
+
+# формируем файл с набором случайных чисел
+with open('numbers.txt', 'w') as f:
+    f.write(' '.join([str(randint(0, 100)) for i in range(randint(2, 100))]))
+
+with open('numbers.txt') as f:
+    try:
+        array = [int(n) for n in f.readline().split()]
+        print(array)
+        print(f'Sum: {sum(array)}')
+    except ValueError:
+        print('Incorrect value')
