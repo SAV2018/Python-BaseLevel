ab3aa94018421aeef1600f80e6ca76b1e43fdd3d
Sav <arconar@mail.ru>
2019-11-12 00:04:48 +0300

    Lesson 1-5

--- BL-L5/task-5-6.py
+++ BL-L5/task-5-6.py
@@ -9,3 +9,36 @@
 Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 '''
+
+def sum_hours(hlist):
+    hours = 0
+    for h in hlist:
+        if (h == '—'):
+            continue
+        else:
+            try:
+                hours += int(h)
+            except ValueError:
+                print(f'Incorrect value of hours "{h}"')
+    return hours
+
+filename = 'subjects.lst'
+try:
+    with open(filename) as f:
+        for item in f:
+            print(item)
+
+            # удаляем 
+            signatures = [':', '(л)', '(пр)', '(лаб)']
+            for signature in signatures:
+                item = item.replace(signature, '')
+            subject, *hours = item.split()
+            print(subject, hours)
+
+            print(f'Сумма часов: {sum_hours(hours)}')
+
+
+except FileNotFoundError:
+    print(f'Error: File "{filename}" not found.')
+except IOError:
+    print('Error: input-output error.')
